052644709420aae2a71bb41bad42877e6d1751b9
myoan <motoki.yoan@gmail.com>
2018-07-01 01:00:38 +0900

    Add stack struct

--- main.go
+++ main.go
@@ -2,21 +2,39 @@ package main
 
 import "fmt"
 
+type CalcStack struct {
+  reg [10]int
+  index int
+}
+
+func (st *CalcStack) Result() int {
+  return st.reg[st.index-1]
+}
+
+func (st *CalcStack) Pop() int {
+  result := st.reg[st.index-1]
+  st.index = st.index - 1
+  fmt.Println("Pop:", result)
+  fmt.Println(st)
+  return result
+}
+
+func (st *CalcStack) Push(r int) {
+  st.reg[st.index] = r
+  st.index = st.index + 1
+  fmt.Println("Push:", r)
+  fmt.Println(st)
+}
+
 type CalcType int
 
 const (
 	Int CalcType = iota
-	Opr
+	Add
+    Sub
+    Mul
+    Div
 	Eof
-)
-
-type CalcOpr int
-
-const (
-	Add CalcOpr = iota
-	Sub
-	Mul
-	Div
 )
 
 type Data struct {
@@ -25,29 +43,63 @@ type Data struct {
 }
 
 func Calc(data []Data) int {
-	reg := [10]int{}
-	i := 0
+  reg := CalcStack{index: 0}
 	for _, d := range data {
 		switch d.tipe {
 		case Int:
-			reg[i] = d.value
-			i = i + 1
-		case Opr:
-			a := reg[i-2]
-			b := reg[i-1]
-			reg[i] = a + b
+			reg.Push(d.value)
+		case Add:
+			a := reg.Pop()
+			b := reg.Pop()
+			reg.Push(b + a)
+		case Sub:
+			a := reg.Pop()
+			b := reg.Pop()
+			reg.Push(b - a)
+		case Mul:
+			a := reg.Pop()
+			b := reg.Pop()
+			reg.Push(b * a)
+		case Div:
+			a := reg.Pop()
+			b := reg.Pop()
+			reg.Push(b / a)
 		case Eof:
 			break
 		}
 	}
-	return reg[i]
+	return reg.Result()
 }
 
 func main() {
+  /*
 	data := []Data{
 		{tipe: Int, value: 5},
+		{tipe: Int, value: 2},
+		{tipe: Add, value: 1},
+	}
+    */
+
+    // (5 + 2) * 3
+    /*
+	data := []Data{
+		{tipe: Int, value: 5},
+		{tipe: Int, value: 2},
+		{tipe: Add, value: 1},
+		{tipe: Int, value: 3},
+		{tipe: Mul, value: 1},
+		{tipe: Eof, value: 1},
+	}
+    */
+    // (5 + 2) * (3 - 1)
+	data := []Data{
+		{tipe: Int, value: 5},
+		{tipe: Int, value: 2},
+		{tipe: Add, value: 1},
+		{tipe: Int, value: 3},
 		{tipe: Int, value: 1},
-		{tipe: Opr, value: 1},
+		{tipe: Sub, value: 1},
+		{tipe: Mul, value: 1},
 		{tipe: Eof, value: 1},
 	}
 
